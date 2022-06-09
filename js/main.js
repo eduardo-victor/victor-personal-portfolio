0bbd936ac052b710670bcd6c5149c5db5565fefc
Victor Eduardo Silva da Costa <victorofish@gmail.com>
2022-06-09 20:52:59 -0300

    personal website in development

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active')
+}
+
+
+//Scroll animation
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration:2000,
+    reset: true
+})
+
+sr.reveal('.home-text', {})
+sr.reveal('.home-img', {delay:200})
+
+sr.reveal('.about-img', {})
+sr.reveal('.heading', {})
+sr.reveal('.about-text', {delay:200})
+
+sr.reveal('.services-content', {})
+sr.reveal('.social', {})
